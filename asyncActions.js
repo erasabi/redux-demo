1afc1e79839a3666cecdead94e060c507b480f35
Elan Rasabi <elan.rasabi@tinroofsoftware.com>
2020-09-10 23:28:34 -0400

    #4b async redux store: create api call function that returns funtion (not json) using thunk lib

--- asyncActions.js
+++ asyncActions.js
@@ -1,5 +1,14 @@
 const redux = require("redux")
 const createStore = redux.createStore
+// apply middleware
+const applyMiddleware = redux.applyMiddleware
+// import redux-thunk
+//  - this lib helps easily define ASYNC action creators
+//  - does this by letting you return a function in place of action object
+//      ...which is the only think action creators can return
+const thunkMiddleware = require('redux-thunk').default
+// import axios for api requests
+const axios = require('axios')
 
 const initialState = {
     loading: false,
@@ -50,4 +59,72 @@ const reducer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(reducer)
+// create an api call function that returns an action FUNCTION
+//  - remember we can return a function instead of json cause of 'thunk'
+const fetchUsers = () => {
+    // pass dispatch method to allow dispatch usage in function
+    return function(dispatch) {
+        // call dispatch to set state to fetching 
+        //  - thereby setting state.loading to true
+        dispatch(fetchUsersRequest())
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                // call dispatch to set state to successful 
+                //  - setting state.loading to false
+                //  - setting users = response.data
+                const users = response.data
+                dispatch(fetchUsersSuccess(users))
+            })
+            .catch(error => {
+                // call dispatch to set state to failure
+                //  - setting state.loading to false
+                //  - setting error to error.message
+                dispatch(fetchUsersFailure(error.message))
+            })
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+store.subscribe(() => console.log(store.getState()))
+
+// call the api function you created
+//  - remember you have to call it within dispatch
+store.dispatch(fetchUsers())
+// OUTPUT:
+// { loading: true }
+// {
+//     loading: false,
+//         users: [
+//             {
+//                 id: 1,
+//                 name: 'Leanne Graham',
+//                 username: 'Bret',
+//                 email: 'Sincere@april.biz',
+//                 address: [Object],
+//                 phone: '1-770-736-8031 x56442',
+//                 website: 'hildegard.org',
+//                 company: [Object]
+//             },
+//             {
+//                 id: 2,
+//                 name: 'Ervin Howell',
+//                 username: 'Antonette',
+//                 email: 'Shanna@melissa.tv',
+//                 address: [Object],
+//                 phone: '010-692-6593 x09125',
+//                 website: 'anastasia.net',
+//                 company: [Object]
+//             },
+//             {
+//                 id: 3,
+//                 name: 'Clementine Bauch',
+//                 username: 'Samantha',
+//                 email: 'Nathan@yesenia.net',
+//                 address: [Object],
+//                 phone: '1-463-123-4447',
+//                 website: 'ramiro.info',
+//                 company: [Object]
+//             }
+//         ],
+//             error: ''
+// }
